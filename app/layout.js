b5a5cdef9ce4334cbd7addda5fe74ba9c68eac8a
Wai Ho Choy <choywaiho1@gmail.com>
2023-02-05 23:15:02 -0800

    feat: add tailwind css

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+// https://beta.nextjs.org/docs/styling/tailwind-css
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    // "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
