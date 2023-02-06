ff9f0e95291dfdc4f9beeda064e72aa89d71e71b
Wai Ho Choy <choywaiho1@gmail.com>
2023-02-06 00:57:57 -0800

    chore: add a few console logs to the api endpoint

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -1,16 +1,18 @@
 export default function handler(req, res) {
   // extract body
   const body = req.body;
-  console.log("body: ", body);
+  console.log("/api/chatgpt received: ", body);
 
   // request validation
   if (!body.message) {
+    console.error("/api/chatgpt failed to receive body.message");
     return res.status(400).json({ data: "Message not provided" });
   }
 
   // do stuff
 
   // return successful response
-  const result = "hello there";
+  const result = "ChatGPT result";
+  console.log("/api/chatgpt returning ChatGPT response");
   res.status(200).json({ data: `${result}` });
 }
