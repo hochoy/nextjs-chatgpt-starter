e3df330d5a73b579c7a5c31dadac200ded5b9046
Wai Ho Choy <choywaiho1@gmail.com>
2023-02-06 00:54:09 -0800

    fix: rename class to classNames. refactor/split Messages into MessageHistory, InboundMessage, OutboundMessage

--- app/page.js
+++ app/page.js
@@ -1,123 +1,79 @@
+"use client";
 function ChatRoom() {
   return (
-    <div class="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
+    <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
       <Title text="Demo of Chat GPT (Tailwind CSS)" />
-      <Messages />
+      <MessageHistory />
       <TypeBox />
     </div>
   );
 }
-function Messages() {
+
+function OutboundMessage({ text, mins_ago }) {
   return (
-    <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
-      <div class="flex w-full mt-2 space-x-3 max-w-xs">
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        <div>
-          <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+    <div className="flex w-full mt-2 space-x-3 max-w-xs">
+      <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+      <div>
+        <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+          <p className="text-sm">{text}</p>
         </div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod.
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs">
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        <div>
-          <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt.
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">Lorem ipsum dolor sit amet.</p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs">
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        <div>
-          <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-            <p class="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-            </p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-      </div>
-      <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-        <div>
-          <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-            <p class="text-sm">Lorem ipsum dolor sit.</p>
-          </div>
-          <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-        </div>
-        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+        <span className="text-xs text-gray-500 leading-none">
+          {mins_ago} min ago
+        </span>
       </div>
     </div>
   );
 }
 
+function InboundMessage({ text, mins_ago }) {
+  return (
+    <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+      <div>
+        <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+          <p className="text-sm">{text}</p>
+        </div>
+        <span className="text-xs text-gray-500 leading-none">
+          {mins_ago} min ago
+        </span>
+      </div>
+      <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+    </div>
+  );
+}
+
+function MessageHistory() {
+  return (
+    <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
+      <OutboundMessage
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        mins_ago="2"
+      />
+      <InboundMessage
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod."
+        mins_ago="2"
+      />
+      <InboundMessage
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod."
+        mins_ago="2"
+      />
+    </div>
+  );
+}
+
+// https://nextjs.org/docs/guides/building-forms
 function TypeBox() {
   return (
-    <div class="bg-gray-300 p-4">
+    <div className="bg-gray-300 p-4">
       <input
-        class="flex items-center h-10 w-full rounded px-3 text-sm"
+        className="flex items-center h-10 w-full rounded px-3 text-sm"
         type="text"
+        id="message"
+        name="message"
+        required
+        minLength="5"
+        maxLength="200"
         placeholder="Type your message…"
       />
     </div>
@@ -126,7 +82,7 @@ function TypeBox() {
 
 function CenteredBackground({ children }) {
   return (
-    <div class="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
       {children}
     </div>
   );
@@ -134,18 +90,16 @@ function CenteredBackground({ children }) {
 
 function Title({ text }) {
   return (
-    <div class="bg-gray-300 p-4 rounded">
-      <h1 class="flex text-md px-3 justify-center">{text}</h1>
+    <div className="bg-gray-300 p-4 rounded">
+      <h1 className="flex text-md px-3 justify-center">{text}</h1>
     </div>
   );
 }
 
 export default function Page() {
   return (
-    <>
-      <CenteredBackground>
-        <ChatRoom />
-      </CenteredBackground>
-    </>
+    <CenteredBackground>
+      <ChatRoom />
+    </CenteredBackground>
   );
 }
